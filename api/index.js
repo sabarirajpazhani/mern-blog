7d778b968da86dea832bf4f70d4e8ca731022334
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-05-20 22:38:41 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(
+    ()=>{console.log('MongoDB is Connected')
 
-const app=express();  //initializes an Express application, allowing you to set up a web server by defining routes, middleware, and server configurations.
-
-app.listen(3000,()=>{//starts the Express server on port 3000
+    }).catch((err)=>{
+        console.log(err);
+    });
+const app=express();  
+app.listen(3000,()=>{
     console.log('Server is running on Port 3000!');
 })
